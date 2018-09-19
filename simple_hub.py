79c1a7d63bbdc0edc0002c395c33bf109eef92b4
Jimmy Angel Pérez Díaz <mratom2307@gmail.com>
2018-09-19 11:16:58 -0500

    Add files via upload

---
+++ simple_hub.py
@@ -0,0 +1,55 @@
+﻿#  simple_hub.py
+#
+#  Copyright 2018  Jimmy A. Pérez Díaz
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+import glob
+from gfm import gfmdef load_file(the_file):
+    with open(the_file, 'r') as data:
+        return data.read()
+
+
+def render(text):
+    return gfm.markdown(text)
+
+
+def html_template(text, css='"simple_dark.css"'):
+    html = """
+    <!DOCTYPE HTML>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width,initial-scale=1">
+            <title>SimpleHub(Alpha)</title>
+            <link rel="stylesheet" type="text/css" media="all" href=""" + css + """/>
+        </head>
+        <body>""" + text + """</body>
+    </html>"""
+    return html
+
+
+def export_html(file_html, filename):
+    with open(filename, 'w') as document:
+        document.write(file_html)
+
+
+lista_md = glob.glob('*.md')
+for i in lista_md:
+    md_rendered = render(load_file(i))
+    export_html(html_template(md_rendered), i.replace('.md', '.html'))
+
---
+++ test/github-readme-templates-master/README.md
@@ -0,0 +1,44 @@
+
+Github Readme Templates
+=============
+Create reusable github readme templates.
+
+Table of Contents
+-----------------
+
+-   [Project Background](#project-background)
+-   [Install & Setup](#install-&-setup)
+-   [Usage](#usage)
+-   [Authors](#authors)
+-   [License](#license)
+
+Project Background
+----------
+
+While designing readme documents for our Orthologs project, we decided to create this repository to help others.
+
+
+Install & Setup
+---------------
+
+There is no real install or setup outside of downloading this repository.
+
+
+Usage
+-----
+
+Place code examples here and other ways to use this project/pipeline.
+
+
+
+Authors
+-------
+
+* S. Hutchins | [@sdhutchins](https://github.com/sdhutchins)
+* Robert Gilmore | [@grabear](https://github.com/grabear)
+
+
+License
+-------
+
+[BSD © Datasnakes](LICENSE)
